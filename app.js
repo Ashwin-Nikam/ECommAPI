3de9254ccab62f1700ac55df533cbd6c26abd1c2
Ashwin-Nikam <nikam.ashwin7@gmail.com>
2018-05-10 18:52:10 -0400

    Simple error handling done

--- app.js
+++ app.js
@@ -16,5 +16,25 @@ Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+	const error = new Error('Not found');
+	error.status = 404;
+	next(error); // Throws the error to the next block
+});
+
+/*
+	Catches errors from previous block (if initial route
+	isn't correct) as well as errors coming from 
+	elsewhere.
+*/
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message
+		}
+	});
+});
+
 app.listen(port);
 console.log('Running on port ' + port);
