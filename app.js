729db17b190b4fc4af75c2d333b1d40925f418c9
Ashwin-Nikam <nikam.ashwin7@gmail.com>
2018-05-10 18:41:30 -0400

    Installed morgan for logging

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 /*
 This is the middleware
 Routes which should handle requests
 */
 
-app.use('/', (req, res) => {
-	res.status(200)
-		.send('Please use GET /products or GET /orders');
-});
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
