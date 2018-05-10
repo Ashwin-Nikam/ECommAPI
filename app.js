280c36935284b64c3de6b94a06da062cf1a39e3b
Ashwin-Nikam <nikam.ashwin7@gmail.com>
2018-05-10 19:16:14 -0400

    Parser implemented successfully

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 /*
