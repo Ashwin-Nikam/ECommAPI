37fdc026b7bde974c4d2e2cac159e67ed53d817c
Ashwin-Nikam <nikam.ashwin7@gmail.com>
2018-05-10 16:08:48 -0400

    Setup initial handler

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const productRoutes = require('./api/routes/products.js');
+
 // Our own custom middleware
-app.use((req, res, next) => {
-	res.status(200).json({
-		message: 'It works!'
-	});
-});
+app.use('/products', productRoutes);
 
 app.listen(port);
 console.log('Running on port ' + port);
