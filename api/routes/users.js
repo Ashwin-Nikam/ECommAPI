179a582b07ef7c1b50228a9ee20e71bfd697224d
Ashwin-Nikam <nikam.ashwin7@gmail.com>
2018-05-15 20:08:37 -0400

    Deleted and adding users done successfully

--- api/routes/users.js
+++ api/routes/users.js
@@ -7,30 +7,51 @@ const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
 	/*
-		2nd parameter is number of salting rounds which hashing the password
+		Check if email already signed up
 	*/
-
-	password: bcrypt.hash(req.body.password, 10, (err, hash) => {
-		if(err) {
-			console.log(err);
-			return res.status(500).json({error: err});
-		}
+	User.find({email: req.body.email}).exec()
+	.then(user => {
+		if(user.length >= 1) return res.status(409).json({message: 'email exists'});
 		else {
-			const user = new User({
-				_id: new mongoose.Types.ObjectId(),
-				email: req.body.email,
-				password: hash			
+			/*
+				2nd parameter is number of salting rounds which hashing the password
+			*/
+			password: bcrypt.hash(req.body.password, 10, (err, hash) => {
+				if(err) {
+					console.log(err);
+					return res.status(500).json({error: err});
+				}
+				else {
+					const user = new User({
+						_id: new mongoose.Types.ObjectId(),
+						email: req.body.email,
+						password: hash			
+					});
+					user.save()
+					.then(result => {
+						res.status(201).json({
+							message: 'User created'
+						});
+					})
+					.catch(err => {
+						res.status(500).json({error: err});
+					})
+				}
 			});
-			user.save()
-			.then(result => {
-				res.status(201).json({
-					message: 'User created'
-				});
-			})
-			.catch(err => {
-				res.status(500).json({error: err});
-			})
 		}
+	})
+});
+
+
+router.delete('/:id', (req, res, next) => {
+	User.remove({_id: req.params.id})
+	.exec()
+	.then(result => {
+		res.status(200).json({message: 'User deleted'});
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json({error: err});
 	});
 });
 
