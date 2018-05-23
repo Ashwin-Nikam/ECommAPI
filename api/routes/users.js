62bdf0a615c7c50d08f997e5a6859d7b979e1cef
Ashwin-Nikam <nikam.ashwin7@gmail.com>
2018-05-23 17:48:49 -0400

    Cleaned up imports

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const checkAuth = require('../middleware/check-auth');
 
-const User = require('../models/user');
+const checkAuth = require('../middleware/check-auth');
 const UserController = require('../controllers/users');
 
 router.post('/signup', UserController.user_signup);
